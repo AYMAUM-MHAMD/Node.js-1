299d529b6f7a6d859f7c47d8fad9783abce1de32
ausi <mntskeaese@gmail.com>
2024-02-21 21:08:54 +0200

    12

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 const User = require("./models/customerSchema");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+var moment = require('moment');
 
 const path = require("path");
 const livereload = require("livereload");
@@ -24,7 +25,7 @@ liveReloadServer.server.once("connection", () => {
 app.get("/", (req, res) => {
   User.find()
     .then((result) => {
-      res.render("index", { arr: result });
+      res.render("index", { arr: result, moment: moment });
     })
 
     .catch((arr) => {
@@ -32,16 +33,6 @@ app.get("/", (req, res) => {
     });
 });
 
-app.get("/user/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((result) => {
-      res.render("user/view", {obj: result});
-    })
-
-    .catch((arr) => {
-      console.log(arr);
-    });
-});
 
 app.get("/user/add.html", (req, res) => {
   res.render("user/add");
@@ -49,6 +40,18 @@ app.get("/user/add.html", (req, res) => {
 
 app.get("/user/edit.html", (req, res) => {
   res.render("user/edit");
+});
+
+
+app.get("/user/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => {
+      res.render("user/view", {obj: result, moment : moment});
+    })
+
+    .catch((arr) => {
+      console.log(arr);
+    });
 });
 
 app.post("/user/add.html", (req, res) => {
@@ -62,6 +65,8 @@ app.post("/user/add.html", (req, res) => {
       console.log(arr);
     });
 });
+
+
 
 mongoose
   .connect(
